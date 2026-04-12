e7500e6582e644607d107541b65514b0b0ff21cd
krishna-brv <brahmadevararoopvenkatakrishna@gmail.com>
2026-04-12 14:13:37 +0530

    Update AdminProfile.jsx

--- WEEK7_CAPSTONE_PROJECT/BlogApp/Frontend-BlogApp/src/components/AdminProfile.jsx
+++ WEEK7_CAPSTONE_PROJECT/BlogApp/Frontend-BlogApp/src/components/AdminProfile.jsx
@@ -21,7 +21,7 @@ function AdminProfile() {
   const handleToggleStatus = async (userId, currentStatus) => {
     try {
       await axios.patch(
-        "http://localhost:5000/admin-api/users",
+        "https://atp-24eg112c38-2.onrender.com/admin-api/users",
         {
           userId: userId,
           isUserActive: !currentStatus,
@@ -44,7 +44,7 @@ function AdminProfile() {
       setLoading(true);
       try {
         let res = await axios.get(
-          "http://localhost:5000/admin-api/users",
+          "https://atp-24eg112c38-2.onrender.com/admin-api/users",
           { withCredentials: true }
         );
 
