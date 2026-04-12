c8ea9b8e1d5ef681e7ebc7745e13dc6f448e8cf0
krishna-brv <brahmadevararoopvenkatakrishna@gmail.com>
2026-04-12 14:07:37 +0530

    Update server.js

--- WEEK7_CAPSTONE_PROJECT/BlogApp/Backend-BlogApp/server.js
+++ WEEK7_CAPSTONE_PROJECT/BlogApp/Backend-BlogApp/server.js
@@ -13,7 +13,7 @@ config();
 const app = exp()
 // assign port
 
-app.use(cors({origin:["http://localhost:5173"],credentials:true}))
+app.use(cors({origin:"*",credentials:true}))
 // connect to db
 const connectDB= async()=>{
     try{
