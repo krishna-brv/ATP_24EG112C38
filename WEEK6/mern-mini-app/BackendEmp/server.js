6c4ba7e8a990950069a096df75a4e0f9eb83c5f7
krishna-brv <brahmadevararoopvenkatakrishna@gmail.com>
2026-04-12 10:31:04 +0530

    Update server.js

--- WEEK6/mern-mini-app/BackendEmp/server.js
+++ WEEK6/mern-mini-app/BackendEmp/server.js
@@ -1,35 +1,32 @@
-import exp from 'express'
+import exp from 'express';
 import { empApp } from './APIs/EmployeeAPI.js';
-import {connect} from 'mongoose'
-import cors from 'cors'
-const app=exp()
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-app.use(cors({
-    origin:['http://localhost:5173'],
-}))
-app.use(exp.json())
+const app = exp();
 
-app.use('/emp-api',empApp)
+app.use(cors());
+app.use(exp.json());
 
+app.use('/emp-api', empApp);
 
-async function connectDB(){
-    try{
-        await connect("mongodb://localhost:27017/myDatabase");
-        console.log("DB connection success")
-        // start server
-        const port =5000
-        app.listen(port,()=>console.log("server started on 5000 port"))
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-    }
-    catch(err){
-        console.log("error occured while connecting to dbase",err);
-        
-    }
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.log("error occured while connecting to dbase", err);
+  }
 }
+
 connectDB();
 
-
-//error handling middleware
 app.use((err, req, res, next) => {
   console.log("err in middleware:", err.message);
 
