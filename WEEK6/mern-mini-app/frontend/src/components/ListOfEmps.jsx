112ad3382c165d6a29ea586269f625e546719d91
krishna-brv <brahmadevararoopvenkatakrishna@gmail.com>
2026-04-12 11:15:21 +0530

    Update ListOfEmps.jsx

--- WEEK6/mern-mini-app/frontend/src/components/ListOfEmps.jsx
+++ WEEK6/mern-mini-app/frontend/src/components/ListOfEmps.jsx
@@ -17,7 +17,7 @@ function ListOfEmps() {
     try{
     setLoading(true);
     setError("");
-    let res = await axios.delete(`http://localhost:5000/emp-api/emp/${id}`)
+    let res = await axios.delete(`https://employee-i0iq.onrender.com/emp-api/emp/${id}`)
     if(res.status===200){
       //get the latest data
         getEmps();
@@ -39,7 +39,7 @@ function ListOfEmps() {
     try{
       setLoading(true);
       setError("");
-      let res = await axios.get("http://localhost:5000/emp-api/emp");
+      let res = await axios.get("https://employee-i0iq.onrender.com/emp-api/emp");
       if (res.status === 200) {
         let resObj = res.data;
         setEmps(resObj.payload);
